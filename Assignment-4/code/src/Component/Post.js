367c9f8c91d037c2d4b4dcb800256e9c44e8bd20
Jigyasha Rani <jigrani0000@gmail.com>
2021-12-23 12:29:46 +0530

    correct assignment-4

--- Assignment-4/code/src/Component/Post.js
+++ Assignment-4/code/src/Component/Post.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Post(props) {
     return (
         <div>
-            <div className="card">
+            <div className="card mt-3">
                 <div className="card-header">
                     <nav className="navbar navbar-light bg-light">
                         <div className="container-fluid">
@@ -11,7 +11,7 @@ export default function Post(props) {
                                 <div><h5 className="card-title">{props.obj.name}</h5></div>
                                 <div><h6 className="card-title">{props.obj.location}</h6></div>
                             </p>
-                            <p>...</p>
+                            <p className='navsize'>...</p>
                         </div>
                     </nav>
                 </div>
@@ -26,7 +26,7 @@ export default function Post(props) {
                                     </button>
                                     <div className="collapse navbar-collapse" id="navbarText">
                                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                            <li className="nav-item">
+                                            <li className="nav-item mx-2">
                                                 <div>
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                                                         <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
@@ -36,7 +36,7 @@ export default function Post(props) {
                                                     {props.obj.likes} Likes
                                                 </div>
                                             </li>
-                                            <li className="nav-item">
+                                            <li className="nav-item my-0">
                                                 <a className="nav-link" href="/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
                                                     <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
                                                 </svg></a>
